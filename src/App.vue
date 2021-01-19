5984867f22f64ec373d14327680d1e4a1d1a0f5b
yosuke <00ysk.tech@gmail.com>
2021-01-19 20:00:35 +0900

    initial

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <AppHeader />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<style lang="scss">
+
+</style>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    AppHeader
+  }
+}
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <div class="h">
+      <div class="h__logo">nisestagram</div>
+      <div class="h__icons">
+        <img src="../assets/ico_plus.svg" alt="nice" width="24" height="24" />
+        <img src="../assets/ico_nice.svg" alt="nice" width="24" height="24" />
+        <img src="../assets/ico_send.svg" alt="nice" width="24" height="24" />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+
+  }
+}
+</script>
+
+<style lang="scss">
+  .h {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 8px;
+
+    &__logo {
+      font-family: 'Pacifico';
+      font-size: 24px;
+    }
+
+    &__icons {
+      display: flex;
+
+      img {
+        margin: 0 8px;
+      }
+    }
+  }
+</style>
